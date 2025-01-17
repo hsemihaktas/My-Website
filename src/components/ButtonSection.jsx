10cb2144a1e69f800efc96b273beab30f2f89a9e
Semih <hsemihaktas@gmail.com>
2025-01-17 11:33:53 +0300

    style: update CSS for project button

--- src/components/ButtonSection.jsx
+++ src/components/ButtonSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ButtonSection = ({ project }) => {
   return (
-    <div className="mt-8 flex flex-wrap gap-4">
+    <div className="mt-8 flex flex-wrap gap-4 justify-end">
       {project.codeLink && (
         <button className="text-sm lg:text-base py-2 px-6 text-[#213555] bg-[#F1F1F1] hover:bg-[#4F709C] hover:text-white rounded-full shadow-lg transition duration-300 mb-4 w-full md:w-auto">
           <a
