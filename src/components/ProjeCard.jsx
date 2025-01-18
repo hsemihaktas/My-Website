29be2c0626ad9795a96c6f2b41e23a403e596bde
Semih <hsemihaktas@gmail.com>
2025-01-18 20:15:23 +0300

    fix: make button responsive in project card

--- src/components/ProjeCard.jsx
+++ src/components/ProjeCard.jsx
@@ -14,20 +14,20 @@ const ProjeCard = ({ project }) => {
         <div className="w-full lg:w-3/5 lg:mr-6">
           {project.imageFile && (
             <img
-              className="w-full h-48 object-cover object-top group-hover:object-bottom transition-all transform duration-1000"
+              className="w-full max-h-48 overflow-hidden object-cover aspect-auto object-top group-hover:object-bottom transition-all transform duration-1000"
               src={require(`../images/${project.imageFile}`)}
               alt={project.title}
             />
           )}
         </div>
-        <div className="flex flex-col w-full lg:w-2/5 relative">
+        <div className="flex flex-col w-full lg:w-2/5">
           <h1 className="text-xl font-semibold text-[#213555] mb-2">
             {project.title}
           </h1>
           <ul className="custom-list text-sm text-[#555] space-y-1">
             {project.shortDescription}
           </ul>
-          <div className="absolute bottom-0 right-0">
+          <div className="flex mt-2 justify-end">
             <button
               className="text-sm lg:text-base py-2 px-6 text-[#213555] bg-[#F1F1F1] hover:bg-[#4F709C] hover:text-white rounded-full shadow-lg transition duration-300 mb-4"
               onClick={handleDetailClick}
