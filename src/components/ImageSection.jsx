a644f40af17364709a1cb50a1f7850e095641ef8
Semih <hsemihaktas@gmail.com>
2025-01-18 20:04:08 +0300

    style: update CSS in project detail image

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -8,7 +8,7 @@ const ImageSection = ({ project }) => {
           <img
             src={require(`../images/${project.imageFile}`)}
             alt={project.title}
-            className="w-full h-80 object-contain lg:object-cover lg:object-top rounded-lg mb-6 lg:hover:object-bottom transition-all transform duration-1000"
+            className="w-full h-80 object-contain lg:object-cover lg:object-top rounded-lg lg:hover:object-bottom transition-all transform duration-1000"
           />
         </div>
       )}
