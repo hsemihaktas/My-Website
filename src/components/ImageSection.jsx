be9436d218c9a18113b3a60be8cc2697f5c8a463
Semih <hsemihaktas@gmail.com>
2025-01-18 20:14:29 +0300

    style: update CSS for image section in project detail page

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -8,7 +8,7 @@ const ImageSection = ({ project }) => {
           <img
             src={require(`../images/${project.imageFile}`)}
             alt={project.title}
-            className="w-full h-80 object-contain lg:object-cover lg:object-top rounded-lg lg:hover:object-bottom transition-all transform duration-1000"
+            className="w-full max-h-48 lg:max-h-96 overflow-hidden object-cover object-top rounded-lg hover:object-bottom transition-all transform duration-1000"
           />
         </div>
       )}
