d18a5901bafd700d1cbb3b07ace0ecca92fe6166
Semih <hsemihaktas@gmail.com>
2025-01-20 17:52:55 +0300

    feat: add dark mode color settings to Homepage.jsx and pass darkMode prop

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React from "react";
 import WelcomePage from "../components/WelcomePage.jsx";
-import About from '../components/About.jsx';
-import Education from '../components/Education.jsx';
-import Project from '../components/Project.jsx';
-import Footer from '../components/Footer.jsx'
+import About from "../components/About.jsx";
+import Education from "../components/Education.jsx";
+import Project from "../components/Project.jsx";
+import Footer from "../components/Footer.jsx";
+import ToggleButton from "../components/ToggleButton.jsx";
+import { useDarkMode } from "../context/DarkModeContext";
+
 function App() {
+  const { darkMode } = useDarkMode();
   return (
     <>
-      <div className="relative lg:flex h-auto bg-[#FEF3E2] text-[#000000]">
+      <div className="absolute top-4 right-4 z-10">
+        <ToggleButton />
+      </div>
+      <div
+        className={`relative lg:flex h-auto ${
+          darkMode ? "bg-[#1F2937] text-white" : "bg-[#FEF3E2] text-black"
+        }`}
+      >
         <div className="w-full lg:w-1/2 p-4 h-full flex items-center lg:fixed">
-          <WelcomePage />
+          <WelcomePage darkMode={darkMode}/>
         </div>
         <div className="flex flex-wrap h-full w-full lg:w-1/2 p-4 lg:p-20 lg:ml-auto font-medium">
-          <About />
-          <Education />
-          <Project />
+          <About darkMode={darkMode}/>
+          <Education darkMode={darkMode}/>
+          <Project darkMode={darkMode}/>
         </div>
-        <div className='absolute w-full bottom-0 left-0 font-medium'>
-          <Footer />
-          </div>
+        <div className="absolute w-full bottom-0 left-0 font-medium">
+          <Footer darkMode={darkMode}/>
+        </div>
       </div>
-
     </>
   );
 }
