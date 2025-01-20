aca92cde6d14ad29af6c8f0d0dcdc82f518d231b
Semih <hsemihaktas@gmail.com>
2025-01-20 17:54:25 +0300

    feat: add dark mode color settings to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Footer = () => {
+const Footer = ({ darkMode }) => {
+
   return (
-    <footer className="bg-[#D8C4B6] text-white py-4">
+    <footer className={`py-4 ${
+      darkMode ? "bg-[#1F2937] text-white" : "bg-[#FEF3E2] text-black"
+    }`}>
       <div className="container mx-auto">
-        <div className="text-center text-[#213555]">
-          &copy; {new Date().getFullYear()} My Personal Website. All rights reserved.
+        <div className="text-center">
+          &copy; {new Date().getFullYear()} My Personal Website. All rights
+          reserved.
         </div>
       </div>
     </footer>
