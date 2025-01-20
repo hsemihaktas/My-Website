dc84e8dbde1405210c6b122492667a142a3859a6
Semih <hsemihaktas@gmail.com>
2025-01-20 17:53:11 +0300

    feat: add dark mode color settings to ProjectDetail.jsx and pass darkMode prop

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -5,8 +5,12 @@ import ImageSection from "../components/ImageSection";
 import TechnologiesSection from "../components/TechnologiesSection";
 import DescriptionSection from "../components/DescriptionSection";
 import ButtonSection from "../components/ButtonSection";
+import ToggleButton from "../components/ToggleButton";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const ProjectDetail = () => {
+  const { darkMode } = useDarkMode();
+
   const { projectUrl } = useParams();
   const project = data.find((proj) => proj.url === `/projects/${projectUrl}`);
   const navigate = useNavigate();
@@ -14,33 +18,57 @@ const ProjectDetail = () => {
   if (!project) return <div>Project not found</div>;
 
   return (
-    <div className="flex flex-col min-h-screen bg-[#FEF3E2] gap-8">
-      <div className="max-w-screen-2xl m-4 lg:mx-auto bg-[#D8C4B6] p-8 rounded-lg shadow-xl h-auto">
-        <div className="flex items-center mb-4 p-4">
-          <button
-            onClick={() => navigate("/")}
-            className="text-[#213555] hover:text-[#4F709C] mr-4"
-          >
-            <span className="text-4xl">&#8592;</span>
-          </button>
-          <h1 className="text-3xl font-bold text-[#213555]">{project.title}</h1>
-        </div>
-
-        <div className="flex flex-col lg:flex-row gap-8">
-          {/* Sol kısım - Resim */}
-          <div className="w-auto lg:w-1/2 content-center">
-            <ImageSection project={project} />
+    <>
+      <div className="absolute top-4 right-4 z-10">
+        <ToggleButton />
+      </div>
+      <div
+        className={`flex flex-col min-h-screen gap-8 font-medium py-4 lg:py-20 ${
+          darkMode ? "bg-[#1F2937] " : "bg-[#FEF3E2] "
+        }`}
+      >
+        <div
+          className={`max-w-screen-2xl m-4 lg:mx-auto p-8 rounded-lg shadow-xl h-auto
+      ${
+        darkMode
+          ? "bg-[#374151] text-white border-[#4B5563]"
+          : "bg-[#D8C4B6] text-black border-[#D8C4B6] "
+      }`}
+        >
+          <div className="flex items-center mb-4 p-4">
+            <button
+              onClick={() => navigate("/")}
+              className={`hover:text-[#4F709C] mr-4 ${
+                darkMode ? "text-[#8EAED9]" : "text-[#6F4F34]"
+              }`}
+            >
+              <span className="text-4xl">&#8592;</span>
+            </button>
+            <h1
+              className={`text-3xl font-bold ${
+                darkMode ? "text-[#8EAED9]" : "text-[#6F4F34]"
+              }`}
+            >
+              {project.title}
+            </h1>
           </div>
 
-          {/* Sağ kısım - Teknolojiler ve Açıklama */}
-          <div className="w-full lg:w-1/2 text-[#555]">
-            <TechnologiesSection project={project} />
-            <DescriptionSection project={project} />
-            <ButtonSection project={project} />
+          <div className="flex flex-col lg:flex-row gap-8">
+            {/* Sol kısım - Resim */}
+            <div className="w-auto lg:w-1/2 content-center">
+              <ImageSection project={project} />
+            </div>
+
+            {/* Sağ kısım - Teknolojiler ve Açıklama */}
+            <div className="w-full lg:w-1/2">
+              <TechnologiesSection darkMode={darkMode} project={project} />
+              <DescriptionSection darkMode={darkMode} project={project} />
+              <ButtonSection darkMode={darkMode} project={project} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
